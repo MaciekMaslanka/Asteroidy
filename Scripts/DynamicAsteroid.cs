f93b5e609a62e0425c51c6dea9c1ad1b8a9e0d2a
MaciekMaslanka <maciek.maslanka@o2.pl>
2026-06-11 19:39:05 +0200

    Initial commit

---
+++ Scripts/DynamicAsteroid.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class DynamicAsteroid : RigidBody2D
+{
+	public override void _Ready()
+	{
+	}
+	public override void _Process(double delta)
+	{
+	}
+}
